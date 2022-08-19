9d989ab8360aa894990e2d440aaec107d1f4e87b
ppunikim <yd62322@naver.com>
2022-08-19 14:27:18 +0900

    todo

--- spring-boot-book/src/main/java/com/callor/book/controller/HomeController.java
+++ spring-boot-book/src/main/java/com/callor/book/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,9 +21,16 @@ public class HomeController {
     } //alt+insert
 
     @RequestMapping(value="/" , method = RequestMethod.GET)
-    public String home(Model model){
+    public String home(@RequestParam(name="code",required=false,defaultValue = "") String isbn,
+                       Model model){
         List<BookVO> bookList = bookService.selectAll();
-        model.addAttribute("BOOK",bookList);
+
+        if(isbn != null || !isbn.isEmpty()){
+            BookVO bookVO = bookService.findById(isbn);
+            model.addAttribute("BOOK",bookVO);
+            log.debug("findByData{}", bookVO);
+        }
+        model.addAttribute("BOOKS",bookList);
         return "home";
 
     }
--- spring-boot-book/src/main/java/com/callor/book/impl/BookServiceImplV1.java
+++ spring-boot-book/src/main/java/com/callor/book/impl/BookServiceImplV1.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.extern.slf4j.XSlf4j;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -25,10 +27,20 @@ public class BookServiceImplV1 implements BookService {
         return bookList;
     }
 
+
     @Override
     public BookVO findById(String isbn) {
+    /* JPA 의 findById() 는 return type 이 Optional<T> 이다.
+       Optional type 은 데이터가 null 인 경우를 좀 더 쉽게 처리할 수 있도록 도와주는 도구이다.
+     */
+        Optional<BookVO> opBookVO = bookDao.findById(isbn);
 
-        return null;
+        /* Optional 데이터에서 실제 필요한 데이터(bookVO) 를 추출하기 위해
+           get() method 를 사용할 수 있는데, 보통은 orElse() 와 같은 method 를 사용하여
+           데이터(bookVO)가 null 인 경우 다른 값을 생성해 절대 데이터가 null 이 될 수 없도록 처리할 수 있다.
+         */
+        BookVO bookVO = opBookVO.orElse(new BookVO());
+        return bookVO;
     }
 
     @Override
