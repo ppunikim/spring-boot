9fb2c93b73c6399a5c1981558ed2cb58de3c05e9
ppunikim <yd62322@naver.com>
2022-08-23 11:52:02 +0900

    todo

--- spring-boot-todo/src/main/java/com/ppuni/todo/controller/HomeController.java
+++ spring-boot-todo/src/main/java/com/ppuni/todo/controller/HomeController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,15 +17,16 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
+private final TodoService todoService;
+    public HomeController(TodoService todoService) {
 
-    private final TodoDao todoDao;
-    public HomeController(TodoDao todoDao) {
-        this.todoDao = todoDao;
+        this.todoService = todoService;
     }
-
+//Dao 를 바로 호출하기보다, Service 를 호출해서 안정성을 높여준다.
+// 또한 service 에서 새로운 코드를 더 작성할 수 있으므로 Dao 를 바로 호출하지 말자.
     @RequestMapping(value = "/",method= RequestMethod.GET)
     public String home(Model model){
-        List<TodoVO> todoList = todoDao.findAll();
+        List<TodoVO> todoList = todoService.selectAll();
         model.addAttribute("TODO",todoList);
         return "home";
     }
@@ -36,10 +38,15 @@ public class HomeController {
     @RequestMapping(value = "/todo",method = RequestMethod.POST)
     public String todo(TodoVO todoVO){
         log.debug("데이터 {}", todoVO);
-        todoDao.save(todoVO);
+        todoService.insert(todoVO);
 
         return "redirect:/";
     }
+    @RequestMapping(value="/{seq}/delete",method = RequestMethod.GET)
+    public String delete(TodoVO todoVO,@PathVariable("seq") long seq){
+        todoService.delete(seq);
 
+        return "redirect:/";
+    }
 
 }
